3485b600e2e2a830a91f5695ff0fc69eeba4ed29
jessicajackson5 <jjacksdev@gmail.com>
2025-01-08 01:36:45 -0300

    Added CSS import to RandomBook.jsx

--- src/components/RandomBook.jsx
+++ src/components/RandomBook.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './RandomBook.css';
 
 export function RandomBook ({toTitleCase}) { 
     const [book, setBook] = useState(null);
